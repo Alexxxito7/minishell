2570659c6a1d5325288ed1ca91e25e1b040fed31
Rafayel Ghazaryan <rghazary@c3r8p8.42vienna.com>
2025-04-22 15:19:29 +0200

    RG Apr 22 15:19

---
+++ minishell/minishell.h
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rghazary <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 12:42:27 by rghazary          #+#    #+#             */
+/*   Updated: 2025/04/14 12:42:29 by rghazary         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h> 
+# include <sys/stat.h> 
+# include <dirent.h> 
+# include <sys/wait.h>
+# include <signal.h> 
+# include <termios.h> 
+# include <sys/ioctl.h>
+# include <readline/readline.h> 
+# include <readline/history.h>
+# include <term.h>
+# include <stdbool.h>
+
+enum e_struct
+{
+	SUCCESS,
+	ERROR,
+	MALLOC
+};
+
+typedef struct s_input_list
+{
+	char				*str;
+	char				c;
+	struct s_input_list *next;
+}	t_input_list;
+
+typedef struct s_env_list
+{
+	char				*key;
+	char				*value;
+	struct s_env_list	*next;
+}	t_env_list;
+
+typedef struct s_data
+{
+	char				**result;
+	char				*input;
+	int					exit_status;
+	struct s_env_list	env_list;
+}	t_data;
+
+// stirng.c
+
+char	*m_trim(char *input);
+int		m_count_qoute(char *input);
+
+// error.c
+
+void	m_print_status(int status);
+void	m_free(char **str);
+void	m_exit_error(t_data *data, int status);
+
+// env_list.c
+
+t_env_list	*m_new_variable(char *str);
+void		m_init_data(t_data *data, char *input, char **env);
+void		m_add_env_back(t_env_list *lst, t_env_list *new);
+void		m_free_data(t_data *data);
+
+
+//// input_list.c
+void			m_free_input_list(t_input_list *input_list);
+t_input_list	*m_new_input_node(t_input_list *list, char *str, char c);
+
+#endif
